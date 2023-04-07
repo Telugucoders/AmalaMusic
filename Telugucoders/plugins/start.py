bd53ff3ec7c88dfc276d50d221434687c7566fac
yumikobots <126863076+yumikobots@users.noreply.github.com>
2023-04-07 21:28:12 +0530

    Update start.py

--- Telugucoders/plugins/start.py
+++ Telugucoders/plugins/start.py
@@ -56,7 +56,7 @@ async def start_(client: Client, message: Message, _):
            InlineKeyboardButton(_["network_btn"], url=f"https://t.me/{NETWORK}"), 
            ],[
            InlineKeyboardButton(_["git_repo"], url="https://github.com/Telugucoders/AmalaMusic"), 
-           InlineKeyboardButton(_["owner_btn"], url=f"t.me/santhu_afk_bot7981"), 
+           InlineKeyboardButton(_["owner_btn"], url=f"tg://settings"), 
            ],[
            InlineKeyboardButton(_["lang_btn"], callback_data="_langs")
            ]]
